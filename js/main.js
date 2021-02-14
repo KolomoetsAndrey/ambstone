5f9e45c7cd4bef36d80e1e56466face5a1942949
Andrey Kolomoets <kolomoets.and@gmail.com>
2021-02-14 23:38:37 +0200

    mob

--- js/main.js
+++ js/main.js
@@ -12,48 +12,165 @@ $(document).ready(function() {
 	});
 });
 
-// FilterJS
+// Isotope
 $(document).ready(function() {
 
-	var newSelection = "";
-	
-	$("#flavor-nav a").click(function(){
-	
-	    $("#all-flavors").fadeTo(200, 0.10);
-	
-		$("#flavor-nav a").removeClass("current");
-		$(this).addClass("current");
-		
-		newSelection = $(this).attr("rel");
-		
-		$(".flavor").not("."+newSelection).slideUp();
-		$("."+newSelection).slideDown();
-		
-	    $("#all-flavors").fadeTo(600, 1);
-		
+	if ($(window).width() <= '991') {
+		var initial_items = 3;
+	} else {
+		var initial_items = 100;
+	}
+
+	// var initial_items = 3;
+	var next_items = 3;
+	var $grid = $('#grid').isotope({
+		itemSelector: '.element-item',
+		layoutMode: 'masonry',
+		stamp: '.element-item--static'
 	});
+
+	$('.button-group-home').on('click', 'button', function () {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({filter: filterValue});
+		updateFilterCounts();
+	});
+
+	function updateFilterCounts() {
+		var itemElems = $grid.isotope('getFilteredItemElements');
+		var count_items = $(itemElems).length;
 	
+		if (count_items > initial_items) {
+			$('#showMore').show();
+		}
+		else {
+			$('#showMore').hide();
+		}
+		if ($('.element-item').hasClass('visible_item')) {
+			$('.element-item').removeClass('visible_item');
+		}
+		var index = 0;
+
+		$(itemElems).each(function () {
+			if (index >= initial_items) {
+				$(this).addClass('visible_item');
+			}
+			index++;
+		});
+		$grid.isotope('layout');
+	}
+
+	$('.button-group-home').each(function (i, buttonGroup) {
+		var $buttonGroup = $(buttonGroup);
+		$buttonGroup.on('click', 'button', function () {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$(this).addClass('is-checked');
+		});
+	});
+
+	function showNextItems(pagination) {
+		var itemsMax = $('.visible_item').length;
+		var itemsCount = 0;
+		$('.visible_item').each(function () {
+			if (itemsCount < pagination) {
+				$(this).removeClass('visible_item');
+				itemsCount++;
+			}
+		});
+		if (itemsCount >= itemsMax) {
+			$('#showMore').hide();
+		}
+		$grid.isotope('layout');
+	}
+
+	function hideItems(pagination) {
+		var itemsMax = $('.element-item').length;
+		var itemsCount = 0;
+		$('.element-item').each(function () {
+			if (itemsCount >= pagination) {
+				$(this).addClass('visible_item');
+			}
+			itemsCount++;
+		});
+		if (itemsCount < itemsMax || initial_items >= itemsMax) {
+			$('#showMore').hide();
+		}
+		$grid.isotope('layout');
+	}
+	$('#showMore').on('click', function (e) {
+		e.preventDefault();
+		showNextItems(next_items);
+	});
+	hideItems(initial_items);
 });
 
-// GlideJS
-var options = {
-    type: 'slider',
-    gap: 70,
-    perView: 3,
-    peek: -window.innerWidth*0.6,
-    startAt: 0,
-    focusAt: 'center'
-}
+// Slick
+$('.mobile_stages').slick({
+	centerMode: false,
+	centerPadding: '50px',
+	arrows: false,
+	slidesToShow: 1,
+	responsive: [
+		{
+			breakpoint: 991,
+			settings: {
+				centerMode: true,
+				arrows: false,
+				centerPadding: '50px',
+				slidesToShow: 1
+			}
+		}, 
+		{
+			breakpoint: 768,
+			settings: {
+				centerMode: true,
+				arrows: false,
+				centerPadding: '50px',
+				slidesToShow: 1
+			}
+		}
+	]
+});
 
-var instructions = {
-	type: 'slider',
-    gap: 70,
-    perView: 3,
-    peek: -window.innerWidth*0.6,
-    startAt: 0,
-    focusAt: 'center'
-}
+$('.slick').slick({
+	centerMode: true,
+	arrows: false,
+	centerPadding: '150px',
+	slidesToShow: 1,
+	responsive: [
+		{
+			breakpoint: 991,
+			settings: {
+				centerMode: true,
+				arrows: false,
+				centerPadding: '50px',
+				slidesToShow: 1
+			}
+		}, 
+		{
+			breakpoint: 768,
+			settings: {
+				centerMode: true,
+				arrows: false,
+				centerPadding: '50px',
+				slidesToShow: 1
+			}
+		}
+	]
+});
 
-var glide = new Glide('.glide', options).mount();
-var glide = new Glide('.mobile_stages', instructions).mount();
-  
+// Hmaburger & NavbarMobile
+$(document).ready(function() {
+	$('.hamburger').click(function() {
+
+		$mobileNav = $('#menu');
+
+		if ($(this).hasClass('open')) {
+			$mobileNav.css({"width": "0"});
+		}
+		else {
+			$mobileNav.css({"width": "80%"});
+		}
+
+		$(this).toggleClass('open');
+	});
+});
